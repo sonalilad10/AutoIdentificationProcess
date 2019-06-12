8d246708e394253be53e56297cf093d39d9fc584
sonalilad10 <Sonali.Lad@varian.com>
2019-06-12 21:51:44 +0530

    Auto Defaults with Grid View

--- AutoIdentificationProcess/AutoDefaults.aspx
+++ AutoIdentificationProcess/AutoDefaults.aspx
@@ -1,4 +1,10 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="AutoDefaults.aspx.cs" Inherits="AutoIdentificationProcess.AutoDefaults" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <h1>Auto Defaults</h1>
+    <form runat="server">
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="true" AllowPaging="False" OnPageIndexChanging="OnPaging" AutoGenerateEditButton="True">
+        <RowStyle ForeColor="Black" />
+        
+    </asp:GridView>
+    </form>
 </asp:Content>
--- AutoIdentificationProcess/AutoDefaults.aspx.cs
+++ AutoIdentificationProcess/AutoDefaults.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,33 @@ namespace AutoIdentificationProcess
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!this.IsPostBack)
+            {
+                BindGrid();
+            }
+        }
+        protected void BindGrid()
+        {
 
+            SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=AutoIdentificationProcess;Trusted_Connection=true;");
+            SqlCommand cmd = new SqlCommand("select * from dbo.UsersData", con);
+            con.Open();
+
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            //DataTable dt = new DataTable();
+            //sda.Fill(dt);
+            //GridView1.DataSource = dt;
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+            GridView1.DataSource = rdr;
+            GridView1.DataBind();
+            
+            con.Close();
+        }
+        protected void OnPaging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            this.BindGrid();
         }
     }
 }
